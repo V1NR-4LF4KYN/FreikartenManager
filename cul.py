10e38be7bed0a3a189de553ad73f505265bf75b6
James Roper <caturix29@gmail.com>
2021-04-19 10:58:13 +0200

    decided on using json

--- cul.py
+++ cul.py
@@ -8,15 +8,33 @@ import sys
 # -l to convert the argument to lowercase
 
 
+def x():
+    opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
-opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+    if "-c" in opts:
+        print(" ".join(arg.capitalize() for arg in args))
+    elif "-u" in opts:
+        print(" ".join(arg.upper() for arg in args))
+    elif "-l" in opts:
+        print(" ".join(arg.lower() for arg in args))
+    else:
+        raise SystemExit(f"Usage: {sys.argv[0]} (-c | -u | -l) <arguments>...")
 
-if "-c" in opts:
-    print(" ".join(arg.capitalize() for arg in args))
-elif "-u" in opts:
-    print(" ".join(arg.upper() for arg in args))
-elif "-l" in opts:
-    print(" ".join(arg.lower() for arg in args))
-else:
-    raise SystemExit(f"Usage: {sys.argv[0]} (-c | -u | -l) <arguments>...")
+# example
+import json
+
+y = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(y))
