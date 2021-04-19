c97db2793a260a830d61c55a0fb34593a3e523a5
James Roper <caturix29@gmail.com>
2021-04-19 13:49:15 +0200

    minor changes

--- cul.py
+++ cul.py
@@ -8,15 +8,14 @@ import sys
 # -l to convert the argument to lowercase
 
 
-def x():
-    opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
-    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
-    if "-c" in opts:
-        print(" ".join(arg.capitalize() for arg in args))
-    elif "-u" in opts:
-        print(" ".join(arg.upper() for arg in args))
-    elif "-l" in opts:
-        print(" ".join(arg.lower() for arg in args))
-    else:
-        raise SystemExit(f"Usage: {sys.argv[0]} (-c | -u | -l) <arguments>...")
+if "-c" in opts:
+    print(" ".join(arg.capitalize() for arg in args))
+elif "-u" in opts:
+    print(" ".join(arg.upper() for arg in args))
+elif "-l" in opts:
+    print(" ".join(arg.lower() for arg in args))
+else:
+    raise SystemExit(f"Usage: {sys.argv[0]} (-c | -u | -l) <arguments>...")
