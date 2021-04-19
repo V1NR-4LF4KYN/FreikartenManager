f8d36fd5bcaf023c15292580116b9beea783005c
James Roper <caturix29@gmail.com>
2021-04-19 17:50:43 +0200

    Worked on jsonhandling and created json files for both me and anna

--- jsonhandling.py
+++ jsonhandling.py
@@ -1,16 +1,36 @@
 import json
+def addJson():
+  anna = {
+    "name": "Anna",
+    "folge": 0,
+    "freikarten": 0,
+    "youtube": 0,
+    "einmal_aufstehen": 0,
+    "massage": 0,
+    "sklave": 0,
+    "striptease": 0,
+    "hasssache": 0, 
+  }
+  james = {
+    "name": "James",
+    "folge": 0,
+    "freikarten": 0,
+    "youtube": 0,
+    "einmal_aufstehen": 0,
+    "massage": 0,
+    "sklave": 0,
+    "striptease": 0,
+    "hasssache": 0, 
+  }
 
-y = {
-  "name": "John",
-  "age": 30,
-  "married": True,
-  "divorced": False,
-  "children": ("Ann","Billy"),
-  "pets": None,
-  "cars": [
-    {"model": "BMW 230", "mpg": 27.5},
-    {"model": "Ford Edge", "mpg": 24.1}
-  ]
-}
+  # turning python into readable json
+  data_anna = json.dumps(anna, indent=2)
+  data_james = json.dumps(james, indent=2)
 
-print(json.dumps(y))
+  # adding json for james data
+  with open("data_james.json", "a") as file:
+    file.write(data_james)
+
+  # adding json for annas data
+  with open("data_anna.json", "a") as file:
+    file.write(data_anna)
