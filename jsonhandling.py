e41e20ecf7247796e88b51146e21b172526ccc96
V1NR-4LF4KYN <caturix29@gmail.com>
2021-04-20 16:19:42 +0200

    Finished all the json manipulating and letting useres decide who gets a freikarte. Now need to sort of add it to some network. Maybe it can stay in github only... I will look into that

--- jsonhandling.py
+++ jsonhandling.py
@@ -1,31 +1,37 @@
 import json
-def addJson():
-  anna = {
-    "name": "Anna",
-    "folge": 0,
-    "freikarten": 0,
-    "youtube": 0,
-    "einmal_aufstehen": 0,
-    "massage": 0,
-    "sklave": 0,
-    "striptease": 0,
-    "hasssache": 0, 
-  }
-  james = {
-    "name": "James",
-    "folge": 0,
-    "freikarten": 0,
-    "youtube": 0,
-    "einmal_aufstehen": 0,
-    "massage": 0,
-    "sklave": 0,
-    "striptease": 0,
-    "hasssache": 0, 
-  }
 
-  # turning python into readable json
-  data_anna = json.dumps(anna, indent=2)
-  data_james = json.dumps(james, indent=2)
+# data sets, not used yet
+anna = {
+  "name": "Anna",
+  "folge": 0,
+  "freikarten": 0,
+  "youtube": 0,
+  "einmal_aufstehen": 0,
+  "massage": 0,
+  "sklave": 0,
+  "striptease": 0,
+  "hasssache": 0, 
+}
+james = {
+  "name": "James",
+  "folge": 0,
+  "freikarten": 0,
+  "youtube": 0,
+  "einmal_aufstehen": 0,
+  "massage": 0,
+  "sklave": 0,
+  "striptease": 0,
+  "hasssache": 0, 
+}
+
+# turning python into readable json
+data_anna = json.dumps(anna, indent=2)
+data_james = json.dumps(james, indent=2)
+
+
+
+def addJson():
+  global anna, james, data_anna, data_james
 
   # adding json for james data
   with open("data_james.json", "a") as file:
@@ -34,3 +40,14 @@ def addJson():
   # adding json for annas data
   with open("data_anna.json", "a") as file:
     file.write(data_anna)
+  
+
+# func for reseting the data
+def reset():
+  global anna, james, data_anna, data_james
+  with open("data_anna.json", "w") as file:
+    file.write(data_anna)
+
+  with open("data_james.json", "w") as file:
+    file.write(data_james)  
+
--- manipulator.py
+++ manipulator.py
@@ -1,26 +1,133 @@
 # file for manipulating/editing data_james and data_anna
 
-import sys, json
+import sys, json, jsonhandling
 
+# conditions for adding other rewards for certain values of folge
+def checkConditionsAndManipulate_Anna():
+    global data_anna
+
+    # annas conditions
+    if data_anna['folge'] == 0:
+        print('Anna: Deine Folge wurde zurückgesetzt. Streng dich was an!')
+    elif data_anna['folge'] == 1:
+        print('Anna hat 1 Schnapszahl in Folge. Nichts passiert. Viel Glück für die nächste!')
+    elif data_anna['folge'] == 2:
+        print('Die Folge beträgt 2! Du erhältst eine Freikarte')
+        data_anna['freikarten'] += 1
+    elif data_anna['folge'] == 3:
+        print('Es sind nun ganze drei Schnapszahlen hintereinander. Du kriegst einen YouTube-Gutschein!')
+        data_anna['youtube'] += 1
+    elif data_anna['folge'] == 4:
+        print('Langsam langsam... Du hast eine Viererfolge! Du kriegst den Einmal-Aufstehen-Gutschein.')
+        data_anna['einmal_aufstehen'] += 1
+    elif data_anna['folge'] == 5:
+        print('Das gibt eine wohlverdiente Massage. Du hast eine Fünferfolge erreicht!')
+        data_anna['massage'] += 1
+    elif data_anna['folge'] == 6:
+        print('Wieso bist du eigentlich plötzlich so gut?! Du hast 6 in Folge! \nDas gibt einen Sklavengutschein.')
+        data_anna['sklave'] +=1
+    elif data_anna['folge'] == 7:
+        print('Du hast scheinbar eine Siebenerfolge. Aber ich glaube du schummelst... \nDu kriegst einen Striptease, uh lala ;)')
+        data_anna['striptease'] += 1
+    elif data_anna['folge'] == 8:
+        print('Wow! Scheinbar hast du die Folge einmal erfüllt. Eigentlich dachte ich nicht, dass es so weit kommen würde. \nHerzlichen Glückwunsch. \nDeine Folge wird jetzt allerdings auf 0 zurückgesetzt. Auf ein Neues! \nDu kriegst den ultimativen Preis: Du darfst meine Hasssache machen!')
+        data_anna['hasssache'] = 0
+    else:
+        print('Ein Fehler ist wohl unterlaufen. Konsultiere James um ihn zu beheben. \nAchtung merke dir, was passiert ist.')
+        # data_anna['folge'] = 0
+
+
+    # We manipulated the data. Now we need to add it to the json files again.
+    # turn it into json 
+    json_anna = json.dumps(data_anna, indent=2)
+
+    # add it to their file
+    with open("data_anna.json", "w") as file_anna:
+        file_anna.write(json_anna)
+
+
+
+def checkConditionsAndManipulate_James():
+    global data_james
+
+    # james conditions
+    if data_james['folge'] == 0:
+        print('James: Deine Folge wurde zurückgesetzt. Streng dich was an!')
+    elif data_james['folge'] == 1:
+        print('James hat 1 Schnapszahl in Folge. Nichts passiert. Viel Glück für die nächste!')
+    elif data_james['folge'] == 2:
+        print('Die Folge beträgt 2! Du erhältst eine Freikarte')
+        data_james['freikarten'] += 1
+    elif data_james['folge'] == 3:
+        print('Es sind nun ganze drei Schnapszahlen hintereinander. Du kriegst einen YouTube-Gutschein!')
+        data_james['youtube'] += 1
+    elif data_james['folge'] == 4:
+        print('Langsam langsam... Du hast eine Viererfolge! Du kriegst den Einmal-Aufstehen-Gutschein.')
+        data_james['einmal_aufstehen'] += 1
+    elif data_james['folge'] == 5:
+        print('Das gibt eine wohlverdiente Massage. Du hast eine Fünferfolge erreicht!')
+        data_james['massage'] += 1
+    elif data_james['folge'] == 6:
+        print('Wieso bist du eigentlich plötzlich so gut?! Du hast 6 in Folge! \nDas gibt einen Sklavengutschein.')
+        data_james['sklave'] +=1
+    elif data_james['folge'] == 7:
+        print('Du hast scheinbar eine Siebenerfolge. Aber ich glaube du schummelst... \nDu kriegst einen Striptease, uh lala ;)')
+        data_james['striptease'] += 1
+    elif data_james['folge'] == 8:
+        print('Wow! Scheinbar hast du die Folge einmal erfüllt. Eigentlich dachte ich nicht, dass es so weit kommen würde. \nHerzlichen Glückwunsch. \nDeine Folge wird jetzt allerdings auf 0 zurückgesetzt. Auf ein Neues! \nDu kriegst den ultimativen Preis: Du darfst meine Hasssache machen!')
+        data_james['hasssache'] = 0
+    else:
+        print('Ein Fehler ist wohl unterlaufen. Konsultiere James um ihn zu beheben. \nAchtung merke dir, was passiert ist.')
+        # data_james['folge'] = 0
+
+
+    # We manipulated the data. Now we need to add it to the json files again.
+    # turn it into json 
+    json_james = json.dumps(data_james, indent=2)
+
+    # add it to their file
+    with open("data_james.json", "w") as file_james:
+        file_james.write(json_james)
+
+
+
+
+
+
+
+
+######### ACTUAL START OF CODE #########
+
+# getting all users specifications
 opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
 args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
 
 with open("data_anna.json", "r") as f_a: # open  annas json file
-data_anna = json.load(f_a) # read it
+    data_anna = json.load(f_a) # read it
     
 
-with open("data_james.json", "r") as f_a: # open  james json file
-    data_james = json.load(f_a) # read it
+with open("data_james.json", "r") as f_j: # open  james json file
+    data_james = json.load(f_j) # read it
 
-# manipulating the folge, if needed or lets say as orderered
-if "-a" in opts:
-    data_anna.folge += 1
+
+
+# checking for user command specification
+if "-r" in opts:
+    jsonhandling.reset()
     
 elif "-j" in opts:
-    data_james.folge += 1    
+    data_james['folge'] += 1
+    data_anna['folge'] = 0
+    checkConditionsAndManipulate_James()
+    checkConditionsAndManipulate_Anna()
+    
+
+elif "-a" in opts:
+    data_anna['folge'] += 1
+    data_james['folge'] = 0
+    checkConditionsAndManipulate_Anna()
+    checkConditionsAndManipulate_James()
+    
 
 
-
-# conditions for adding other rewards for certain values of folge
-if 
