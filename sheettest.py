68ae821dc80b6914793fee5d71343cb73bb06e87
V1NR-4LF4KYN <caturix29@gmail.com>
2021-11-16 22:13:19 +0100

    Add sheets

--- sheettest.py
+++ sheettest.py
@@ -5,10 +5,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 
-scope = ['https://www.googleapis.com/auth/documents.readonly', "https://www.spreadsheets.google.com/feeds", "https://www.googleapsis.com/auth/spreadsheets", "https://www.googleapsis.com/auth/drive.file", "https://www.googleapsis.com/auth/drive"]
+scope = ['https://www.googleapis.com/auth/documents.readonly', "https://www.spreadsheets.google.com/feeds", "https://www.googleapsis.com/auth/spreadsheets", "https://www.googleapsis.com/auth/drive.file", "https://www.googleapsis.com/auth/drive", "https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/spreadsheets.readonly", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.readonly", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 
+sheet = client.open("FreikartenManager")
 '''
 sheet = client.open("FreikartenManager").sheet1
 data = sheet.get_all_records()
